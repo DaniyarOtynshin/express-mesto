fdc7eb853dc966d88870d2943aa59e79d8e348a8
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-02-07 23:35:31 +0600

    feat: return 404 status for wrong requests

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ const cardsRouter = require('./routes/cards');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT);
