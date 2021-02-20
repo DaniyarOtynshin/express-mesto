32f75cc4815b0cda285b27457508721f53252d99
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-02-20 17:23:08 +0600

    add mongoose models

---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema, ObjectId } = mongoose;
+
+const cardSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+  },
+  owner: {
+    type: [ObjectId],
+    default: [],
+  },
+  likes: {
+    type: Array,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('user', cardSchema);
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
