266288fda7180a7f2f005e81ef88b1b5a165a7d6
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-02-24 17:14:25 +0600

    delete: console.logs

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-})
-  .then(() => {
-    console.log('Connected to DB');
-  });
+});
 
 const PORT = 3000;
 const usersRouter = require('./routes/users');
